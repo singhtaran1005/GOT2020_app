6c686ba9467d35c531cf0b296ea8a3559f90874c
Taranpreet Singh Chabbra <53126276+singhtaran1005@users.noreply.github.com>
2020-03-30 17:59:00 +0530

    updated

--- lib/got_1.dart
+++ lib/got_1.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 Color mainColor = Colors.amber;
 Color secColor = Colors.black;
@@ -9,14 +12,35 @@ class Got1 extends StatefulWidget {
 }
 
 class _Got1State extends State<Got1> {
-
-@override
+  var data;
+  @override
   void initState() {
     super.initState();
     fetchData();
   }
-  fetchData()async{
-    var res = await 
+
+  fetchData() async {
+    var res = await http.get(
+        "http://api.tvmaze.com/singlesearch/shows?q=game-of-thrones&embed=episodes");
+    print(res.body);
+    data = jsonDecode(res.body);
+    setState(() { 
+    });
+  }
+
+  dataBody(BuildContext context){
+    var imgUrl = data["image"]["original"];
+    var body = Column(
+      children: <Widget>[
+      Center(
+        child: CircleAvatar(
+          backgroundImage: NetworkImage("imgUrl"),
+          radius: 70,
+        ),
+      ),
+    ],
+    );
+    return body;
   }
 
   @override
@@ -27,6 +51,11 @@ class _Got1State extends State<Got1> {
         elevation: 0.0,
         title : Center(child: Text("GOT 2020")),
       ),
-    );
+      body: data != null
+      ? dataBody(context)
+      :Center(
+        child: CircularProgressIndicator(),
+        ),
+      );
   }
 }
